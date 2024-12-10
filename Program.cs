d778c533acc35bae296b3144ec8a29eec0d7c626
DenisKazmirchuk <161360416+DenisKazmirchuk@users.noreply.github.com>
2024-12-10 20:35:14 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        string[] words = { "математика", "інформатика", "фізика", "хімія", "література" };
+
+        // Запит на пошук слова
+        var result = words.FirstOrDefault(w => w == "інформатика");
+        Console.WriteLine(result != null ? $"Знайдено слово: {result}" : "Слово не знайдено");
+    }
+}
